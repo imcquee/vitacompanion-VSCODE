edaaee6f1b6dc48c8d148b789ef97b6ccc8c53e0
Jakub Ksiezniak <jksiezni@users.noreply.github.com>
2020-03-14 18:29:10 +0100

    Replace vscode-cache with VSCode Configuration API
    The extension properties now appear in the Settings window and 'settings.json',
    so user can manually change them, both globaly and locally in the workspace.
    
    A new command (Set Target Path) was created, to reflect new extension property.
    
    Improved operation of some commands to be more user friendly.

--- src/extension.js
+++ src/extension.js
@@ -1,6 +1,5 @@
 const vscode = require('vscode');
 const Netcat = require('node-netcat');
-const Cache = require('vscode-cache');
 const FtpDeploy = require("ftp-deploy");
 const PromiseFtp = require('promise-ftp');
 const utils = require('./functions');
@@ -11,111 +10,143 @@ function activate(context) {
 
     console.log('Congratulations, your extension "vitacompanion" is now active!');
 
+    let configuration = vscode.workspace.getConfiguration("vitacompanion")
     var watcher = 0;
     var watcher2 = 0;
     var vpk = 0;
     var ftp = new PromiseFtp();
-    let IP_cache = new Cache(context);
     var ftpDeploy = new FtpDeploy();
-    var client = 0;
-    var fpath = 0;
-    var ip_addr = 0;
-    var rRoot = 0;
-    var target = 0;
+    var client = undefined;
     var DMODE = 0;
     var SMODE = 0;
     var eboot = 0;
 
-    if(IP_cache.has('ip_addr')){
-        ip_addr = IP_cache.get('ip_addr');
-        client = new Netcat.client(1338, ip_addr);
+    function ipAddress() {
+        return vscode.workspace.getConfiguration("vitacompanion").get("ipAddress")
     }
 
-    if(IP_cache.has('path')) fpath = IP_cache.get('path');
-    
-    
+    function getSourcePath() {
+        return vscode.workspace.getConfiguration("vitacompanion").get('ftp.sourcePath');
+    }
+
+    function getTargetPath() {
+        return vscode.workspace.getConfiguration("vitacompanion").get('ftp.targetPath');
+    }
+
+    if(ipAddress()) {
+        client = new Netcat.client(1338, ipAddress());
+    }
+
     let connect_vita = vscode.commands.registerCommand('extension.connect', function () {
-        vscode.window.showInputBox({ prompt: 'Enter Your Vita IP - (Format) x.x.x.x' }).then(code => {
-            IP_cache.put('ip_addr', code);
-            ip_addr = code;
-            client = new Netcat.client(1338, ip_addr);
+        vscode.window.showInputBox({ prompt: 'Enter Your Vita IP - (Format) x.x.x.x', value: ipAddress() }).then(address => {
+            if (address != undefined) {
+                configuration.update('ipAddress', address);
+                client = new Netcat.client(1338, address);
+            }
         }) 
         
     });
 
     let reboot_vita = vscode.commands.registerCommand('extension.reboot', function () {
-        if(!ip_addr) vscode.window.showInformationMessage("Connect to the Vita first using command >Vita: Connect")
+        if(!client) vscode.window.showInformationMessage("Connect to the Vita first using command >Vita: Connect")
         else {
             utils.rbt(client)
         }
     });
 
     let screen_on = vscode.commands.registerCommand('extension.screenOn', function () {
-        if(!ip_addr) vscode.window.showInformationMessage("Connect to the Vita first using command >Vita: Connect")
+        if(!client) vscode.window.showInformationMessage("Connect to the Vita first using command >Vita: Connect")
         else { 
             utils.wake(client);
         }
 
     });
     let screen_off = vscode.commands.registerCommand('extension.screenOff', function () {
-        if(!ip_addr) vscode.window.showInformationMessage("Connect to the Vita first using command >Vita: Connect")
+        if(!client) vscode.window.showInformationMessage("Connect to the Vita first using command >Vita: Connect")
         else {
             utils.Soff(client);
         }
     });
     let launch_vita = vscode.commands.registerCommand('extension.launch', function () {
-        if(!ip_addr) vscode.window.showInformationMessage("Connect to the Vita first using command >Vita: Connect")
+        if(!client) vscode.window.showInformationMessage("Connect to the Vita first using command >Vita: Connect")
         else {
             vscode.window.showInputBox({ prompt: 'Enter Application ID:' }).then(code => {
-                utils.launch(ip_addr,code,client,ftp);
+                utils.launch(ipAddress(),code,client,ftp);
             })
         }
     });
     let destroy_vita = vscode.commands.registerCommand('extension.destroy', function() {
-        if(!ip_addr) vscode.window.showInformationMessage("Connect to the Vita first using command >Vita: Connect")
+        if(!client) vscode.window.showInformationMessage("Connect to the Vita first using command >Vita: Connect")
         else {
             utils.terminate(client);
         }
     });
 
     let send_vita = vscode.commands.registerCommand('extension.send', function() {
-        if(!ip_addr) vscode.window.showInformationMessage("Connect to the Vita first using command >Vita: Connect")
-        else if(!fpath) vscode.window.showInformationMessage("Please register a source first using command >Vita: Set Path")  
+        let path = getSourcePath()
+        if(!client) vscode.window.showInformationMessage("Connect to the Vita first using command >Vita: Connect")
+        else if(!path) vscode.window.showInformationMessage("Please register a source first using command >Vita: Set Path")
+        else if(!getTargetPath()) vscode.window.showInformationMessage("Please set a target folder first using command >Vita: Set Target Path")
         else {
-            vscode.window.showInputBox({ prompt: 'Enter the directory on your device where you want your files to be sent ex. ux0:/Vpks' }).then(code => {
-                rRoot = code;
-                vscode.window.showInputBox({ prompt: 'Enter name of the file you want to send ex. target.vpk' }).then(code => {
-                    target = code;
-                    utils.fsend(rRoot,fpath,ip_addr,target,ftpDeploy);
-                }); 
-            });
-        }        
+            //console.log("path Uri " + vscode.Uri.file(path));
+            vscode.workspace.fs.readDirectory(vscode.Uri.file(path)).then(elements => {
+                //console.log("elements " + elements);
+                let items = elements.filter(e => e[1] == vscode.FileType.File)
+                    .map(e => e[0])
+                //console.log("items " + items);
 
-
-        
-    });
-
-    let set_path = vscode.commands.registerCommand('extension.setpath', function() {
-        if(!ip_addr) vscode.window.showInformationMessage("Connect to the Vita first using command >Vita: Connect")
-        else{
-            vscode.window.showInputBox({ prompt: 'Enter the full directory of your project ex. Users/Documents/myproject' }).then(code => {
-                fpath = code;
-                IP_cache.put('path',fpath);
+                let previousFile = context.workspaceState.get("previousFile");
+                //console.log("previousFile " + previousFile);
+                vscode.window.showQuickPick(items, {
+                    placeHolder: previousFile
+                }).then(picked => {
+                    //console.log("picked " + picked);
+                    if (picked) {
+                        context.workspaceState.update("previousFile", picked)
+                        utils.fsend(getTargetPath(), path, ipAddress(), picked, ftpDeploy)
+                    }
+                });
             });
         }
     });
 
+    let set_path = vscode.commands.registerCommand('extension.setpath', function() {
+        vscode.window.showOpenDialog({
+             openLabel: 'Choose Source Directory',
+             defaultUri: vscode.workspace.workspaceFolders[0].uri,
+             canSelectFiles: false,
+             canSelectFolders: true
+     }).then(dirs => {
+            console.log("change source path to " + dirs[0].fsPath);
+            if (dirs != undefined) {
+                configuration.update("ftp.sourcePath", dirs[0].fsPath);
+            }
+        });
+    });
+
+    let set_target_path = vscode.commands.registerCommand('extension.settargetpath', function() {
+        vscode.window.showInputBox({ 
+            prompt: 'Enter the directory on your device where you want your files to be sent ex. ux0:/Vpks',
+            value: getTargetPath()
+         }).then(targetPath => {
+            console.log("change target path to " + targetPath)
+            configuration.update("ftp.targetPath", targetPath);
+        });
+    });
+
     let full_deploy = vscode.commands.registerCommand('extension.fulldeploy', function() {
-        if(!ip_addr) vscode.window.showInformationMessage("Connect to the Vita first using command >Vita: Connect")
-        else if(!fpath) vscode.window.showInformationMessage("Please register a source first using command >Vita: Set Path") 
+        if(!client) vscode.window.showInformationMessage("Connect to the Vita first using command >Vita: Connect")
+        else if(!getSourcePath()) vscode.window.showInformationMessage("Please register a source first using command >Vita: Set Path") 
         else{
-            utils.fdeploy(fpath,ip_addr,client,ftpDeploy,ftp);
+            utils.fdeploy(getSourcePath(),ipAddress(),client,ftpDeploy,ftp);
         }
     });
 
 
     let debug_mode = vscode.commands.registerCommand('extension.debugmode', function() {
-        if(!ip_addr) vscode.window.showInformationMessage("Connect to the Vita first using command >Vita: Connect")
+        let fpath = getSourcePath();
+        let ip_addr = ipAddress();
+        if(!client) vscode.window.showInformationMessage("Connect to the Vita first using command >Vita: Connect")
         else if(!fpath) vscode.window.showInformationMessage("Please register a source first using command >Vita: Set Path") 
         else{
             async function debug_init() {
@@ -158,22 +189,18 @@ function activate(context) {
     });
 
     let stay_on = vscode.commands.registerCommand('extension.stayon', function() {
-        if(!ip_addr) vscode.window.showInformationMessage("Connect to the Vita first using command >Vita: Connect")
+        if(!client) vscode.window.showInformationMessage("Connect to the Vita first using command >Vita: Connect")
         else{
             SMODE = utils.survive(client,SMODE);
         }
     });
-
-
 
     if(client) client.on('data', function (data) {
         if(!SMODE) vscode.window.showInformationMessage('Received: ' + data); 
     });
 
 
-
-
-    context.subscriptions.push(connect_vita,reboot_vita,screen_on,screen_off,launch_vita,destroy_vita,set_path,send_vita,full_deploy,debug_mode,stay_on);
+    context.subscriptions.push(connect_vita,reboot_vita,screen_on,screen_off,launch_vita,destroy_vita,set_path,send_vita,full_deploy,debug_mode,stay_on,set_target_path);
 }
 exports.activate = activate;
 
